b2ed1810ac8ebed59a660498b2ec061149c93a59
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-01-12 19:24:04 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,38 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
+import MovieStart from "components/MovieStart";
 import Pagination from "components/Pagination";
 import React from "react";
 
-function Listing(){
+function Listing() {
+  return (
+    <>
+      <Pagination />
 
-    return(
-    <Pagination/>
-    );
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
