eaac9178e3bba055bc095518473e352912340030
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-01-13 18:31:36 -0300

    Domain model , database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId  //Id Composto
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	public Score() {
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie); // Associar o Filme com o Score
+	}
+
+	public void setUser(User user) {
+		id.setUser(user); // Associar o Usuario com o Score
+	}
+	
+	public ScorePk getId() {
+		return id;
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+}
