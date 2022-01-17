9b515dfba3a3d4c43b69001a49ca20ad1699e5ca
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-01-16 23:33:03 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,23 +1,35 @@
-import { ReactComponent as Arrow} from 'assets/img/arrow.svg'
+import { ReactComponent as Arrow } from "assets/img/arrow.svg";
 import React from "react";
-import './styles.css'
+import { MoviePage } from "types/movie";
+import "./styles.css";
 
+type Props = {
+  page: MoviePage;
+  onChage: Function;
+};
 
-function Pagination (){
-
-    return(
-<div className="dsmovie-pagination-container">
-    <div className="dsmovie-pagination-box">
-        <button className="dsmovie-pagination-button" disabled={true} >
-            <Arrow />
+function Pagination({ page, onChage }: Props) {
+  return (
+    <div className="dsmovie-pagination-container">
+      <div className="dsmovie-pagination-box">
+        <button
+          className="dsmovie-pagination-button"
+          disabled={page.first}
+          onClick={() => onChage(page.number - 1)}
+        >
+          <Arrow />
         </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="dsmovie-pagination-button" disabled={false} >
-            <Arrow className="dsmovie-flip-horizontal" />
+        <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+        <button
+          className="dsmovie-pagination-button"
+          disabled={page.last}
+          onClick={() => onChage(page.number + 1)}
+        >
+          <Arrow className="dsmovie-flip-horizontal" />
         </button>
+      </div>
     </div>
-</div>
-    );
+  );
 }
 
-export default Pagination
+export default Pagination;
